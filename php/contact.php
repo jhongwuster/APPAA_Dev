81293e7e83bc6cb0378ed4aeb767d64c86b864ac
jhongwuster <jhongwuster@gmail.com>
2016-05-08 09:18:11 +1000

    Contact sending email from user ok

--- php/contact.php
+++ php/contact.php
@@ -1,18 +1,34 @@
 <?php
-
+require_once "PHPMailerAutoload.php";
 @$name = $_POST['name'];
 @$email = $_POST['email'];
-@$address = $_POST['message'];
+@$message = $_POST['message'];
+@$phone = $_POST['mobile'];
 
-
-echo $name AND $email AND $phone AND $address AND $city AND $cuntery AND $code;
-
-$to = 'ahmederaqi5@gmail.com';
-$subject = 'the subject';
-$message = "name = : $name <br/> email is : $email <br/>phone: $phone <br/> address:  $address <br/> city : $city <br/>country : $cuntery <br/> code: $code  ";
-$headers = 'From: webmaster@example.com' . "\r\n" .
-        'Reply-To: webmaster@example.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
+echo $name AND $email AND $phone;
+ 
+   $mail = new PHPMailer();
+   $mail->IsSMTP();
+   $mail->Mailer = "smtp";
+   $mail->Host = "smtp.qq.com";
+   $mail->Port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
+   $mail->SMTPAuth = true;
+   $mail->SMTPSecure = 'ssl';
+   $mail->Username = "admin@apgaa.com.au";
+   $mail->Password = "Wcon6666";
+   $mail->SetFrom($email, $name);
+   $mail->AddAddress("admin@apgaa.com.au", "Customer email");
+   $mail->AddReplyTo($email, $name);
+ 
+   $mail->Subject  = "Message from APGAA customer";
+   $mail->Body     = $message;
+   $mail->WordWrap = 50;  
+ 
+   if(!$mail->Send()) {
+		echo 'Message was not sent.';
+		echo 'Mailer error: ' . $mail->ErrorInfo;
+		exit;
+   } else {
+		echo 'Message has been sent.';
+   }
 ?>
