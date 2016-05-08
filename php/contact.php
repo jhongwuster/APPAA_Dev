d5dcba13c9ff922b08a1d87f91fe2df0d1226c6f
jhongwuster <jhongwuster@gmail.com>
2016-05-08 21:02:23 +1000

    Update code
    1 Update smtp email config
    2 Update navi bar link paramenter

--- php/contact.php
+++ php/contact.php
@@ -10,15 +10,14 @@ echo $name AND $email AND $phone;
    $mail = new PHPMailer();
    $mail->IsSMTP();
    $mail->Mailer = "smtp";
-   $mail->Host = "smtp.qq.com";
-   $mail->Port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
+   $mail->Host = "smtp.exmail.qq.com";
+   $mail->Port = 465; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
    $mail->SMTPAuth = true;
    $mail->SMTPSecure = 'ssl';
    $mail->Username = "admin@apgaa.com.au";
    $mail->Password = "Wcon6666";
-   $mail->SetFrom($email, $name);
+   $mail->SetFrom("admin@apgaa.com.au", $name);
    $mail->AddAddress("admin@apgaa.com.au", "Customer email");
-   $mail->AddReplyTo($email, $name);
  
    $mail->Subject  = "Message from APGAA customer";
    $mail->Body     = $message;
