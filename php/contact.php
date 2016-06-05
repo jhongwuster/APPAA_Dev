ca455ff630a1bc9ca0f48f3d562bd1fdb4ceb277
jhongwuster <jhongwuster@gmail.com>
2016-06-05 22:08:26 +1000

    update mail php

--- php/contact.php
+++ php/contact.php
@@ -3,26 +3,22 @@ require_once "PHPMailerAutoload.php";
 @$name = $_POST['name'];
 @$email = $_POST['email'];
 @$message = $_POST['message'];
-@$phone = $_POST['mobile'];
-
-echo $name AND $email AND $phone;
+@$mobile = $_POST['mobile'];
  
    $mail = new PHPMailer();
    $mail->IsSMTP();
    $mail->Mailer = "smtp";
-   $mail->Host = "smtp.qq.com";
-   $mail->Port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
+   $mail->Host = "smtp.exmail.qq.com";
+   $mail->Port = 465; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
    $mail->SMTPAuth = true;
    $mail->SMTPSecure = 'ssl';
    $mail->Username = "admin@apgaa.com.au";
    $mail->Password = "Wcon6666";
-   $mail->SetFrom($email, $name);
+   $mail->SetFrom("admin@apgaa.com.au", $name);
    $mail->AddAddress("admin@apgaa.com.au", "Customer email");
-   $mail->AddReplyTo($email, $name);
  
    $mail->Subject  = "Message from APGAA customer";
-   $mail->Body     = $message;
-   $mail->WordWrap = 50;  
+   $mail->Body     = "Hi Admin, \nYou have a message from APGAA website:\nSender:".$name."\nMobile:".$mobile."\nEmail:".$email."\nMessage:".$message;
  
    if(!$mail->Send()) {
 		echo 'Message was not sent.';
